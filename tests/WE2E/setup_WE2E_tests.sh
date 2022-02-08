888d827b1e35c0bada479e8d1a24c4fb379d9a30
Christina Holt <56881914+christinaholtNOAA@users.noreply.github.com>
2022-02-08 09:24:29 -0600

    rrfs_ci: Updating test automation script (#275)
    * Modify automation wrapper.
    
    * Adding placeholder machine suite files.
    
    * Update comment to match change in code.
    
    * Rename script.
    
    * Fix spelling errors.
    
    * Minor testing mods.
    
    * Acutally use the configured bin dir.

--- tests/WE2E/setup_WE2E_tests.sh
+++ tests/WE2E/setup_WE2E_tests.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -l
 
 #----------------------------------------------------------------------
-#  Automation of UFS Short Range Weather App Worfklow End to End Tests.
+#  Wrapper for the automation of UFS Short Range Weather App Workflow
+#  End to End Tests.
 #
-#  The script is dependant on a successful build of this repo using the
-#  test/build.sh script in the ufs-srweather-app repository.
-#  The UFS build must be completed in a particular manner for this 
-#  script to function properly, notably the location of the build and
-#  bin directories: 
+#  The wrapper loads the appropriate workflow environment for the
+#  machine, and sets the machine test suite file before invoking the
+#  run_WE2E_tests.sh.
+#
+#  The script is dependent on a successful build of this repo using the
+#  test/build.sh script in the ufs-srweather-app repository.  The UFS
+#  build must be completed in a particular manner for this script to
+#  function properly, notably the location of the build and bin
+#  directories: 
 #    BUILD_DIR=${APP_DIR}/build_${compiler}
 #    BIN_DIR=${APP_DIR}/bin_${compiler}
-#
-#  With the creating of a test_log directory and output files describing
-#  the result of each workflow status, a testing framework is established.
-#  The user must verify the contents of each experiment file in test_log/ 
-#  to check workflow success or failure.
 #
 #  Example: ./end_to_end_tests.sh hera zrtrr
 #----------------------------------------------------------------------
@@ -22,8 +22,6 @@
 #-----------------------------------------------------------------------
 #  Set variables
 #-----------------------------------------------------------------------
-
-branches=( rrfs_ci )
 
 function usage {
   echo
@@ -37,26 +35,19 @@ function usage {
 
 }
 
-machines=( hera )
+machines=( hera jet )
 
 if [ "$1" = "-h" ] ; then usage ; fi
 [[ $# -le 1 ]] && usage
 
-export machine=$1
+machine=$1
 machine=$(echo "${machine}" | tr '[A-Z]' '[a-z]')  # scripts in sorc need lower case machine name
 
-export account=$2
-
-#-----------------------------------------------------------------------
-# Choose experiment.
-#-----------------------------------------------------------------------
-
-expts=( grid_RRFS_CONUS_25km_ics_FV3GFS_lbcs_FV3GFS_suite_GSD_SAR )
+account=$2
 
 #-----------------------------------------------------------------------
 # Set directories
 #-----------------------------------------------------------------------
-
 scrfunc_fp=$( readlink -f "${BASH_SOURCE[0]}" )
 scrfunc_fn=$( basename "${scrfunc_fp}" )
 scrfunc_dir=$( dirname "${scrfunc_fp}" )
@@ -66,32 +57,35 @@ REGIONAL_WORKFLOW_DIR=$( dirname "${TESTS_DIR}" )
 SRW_APP_DIR=$( dirname "${REGIONAL_WORKFLOW_DIR}" )
 TOP_DIR=$( dirname "${SRW_APP_DIR}" )
 
-#BRANCH_DIR_NAME=ufs-srweather-app-${branches}
-APP_DIR=${TOP_DIR}/ufs-srweather-app
 EXPTS_DIR=${TOP_DIR}/expt_dirs
 
+#-----------------------------------------------------------------------
+# Set the path to the machine-specific test suite file.
+#-----------------------------------------------------------------------
+
+auto_file=${scrfunc_dir}/machine_suites/${machine}.txt
+
 #----------------------------------------------------------------------
-# Temporary fix to set EXECDIR in setup.sh appropriately.
+# Use exec_subdir consistent with the automated build.
 #----------------------------------------------------------------------
 
-sed -i 's|EXECDIR="${SR_WX_APP_TOP_DIR}/bin"|EXECDIR="${SR_WX_APP_TOP_DIR}/bin_intel/bin"|g' "${REGIONAL_WORKFLOW_DIR}/ush/setup.sh"
+exec_subdir='bin_intel/bin'
 
 #-----------------------------------------------------------------------
 # Run E2E Tests
 #-----------------------------------------------------------------------
 
 # Load Python Modules
-cmd="${APP_DIR}/env/wflow_${machine}.env"
-source ${cmd}
+env_file="${SRW_APP_DIR}/env/wflow_${machine}.env"
+source ${env_file}
+echo "-- Load environment =>" $env_file
 
-echo "-- Load environment =>"  $cmd
-
-# If experiments list file exists, remove it, and add the experiemnts to a new expts list file
-auto_file="auto_expts_list.txt"
-
-rm -rf ${auto_file}
-echo ${expts} > ${auto_file}
+module list
 
 # Run the E2E Workflow tests
-./run_WE2E_tests.sh tests_file=${auto_file} machine=${machine} account=${account}
+./run_WE2E_tests.sh \
+  tests_file=${auto_file} \
+  machine=${machine} \
+  account=${account} \
+  exec_subdir=${exec_subdir}
 
